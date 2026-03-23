ac110ef07819c34ac25cd0dd1ddd685e71fce62a
1OOyan <43534809+1OOyan@users.noreply.github.com>
2026-03-23 20:44:22 +0200

    Add package init

---
+++ __init__.py
@@ -0,0 +1,7 @@
+"""Structured File Editor - A git-backed file editing system."""
+
+from .structured_editor import StructuredEditor
+
+__version__ = '1.0.0'
+__author__ = 'Structured Editor'
+__all__ = ['StructuredEditor']
